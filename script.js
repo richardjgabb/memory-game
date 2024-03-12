14b2b953f1ff4a4d36b837efc27be001e1445c91
Richard Gabb <richardjgabb@gmail.com>
2024-03-12 09:35:31 +0000

    Button checker

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ closeBtn.addEventListener('click', () => {
     modal.classList.remove('open');
 });
 
-
 const openInstructionsModal = () => {
     modal.classList.add('open');
 }
 
 instructionsButton.addEventListener('click', openInstructionsModal);
+
+//
+let patternCounter = 0;
+const pattern = [3, 8, 1, 2, 7, 4, 6, 9, 1, 6]
+
+let boxes = document.querySelectorAll('.box')
+for (let box of boxes) {
+    box.addEventListener('click', () => {
+        if (box.id == pattern[patternCounter]) {
+            console.log('correct')
+            patternCounter ++;
+        } else {
+            console.log('Game over');
+        }
+    })
+}
+
+
+
 
